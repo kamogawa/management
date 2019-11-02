d8a8e96cd975a15f2f4795dadbb97d67907f8b31
BEUMGOO <captain_kbg@naver.com>
2019-11-02 23:58:00 +0900

    modal customer add / delete

--- src/components/CustomerDelete/index.js
+++ src/components/CustomerDelete/index.js
@@ -1,7 +1,33 @@
 import React from 'react';
-import { Button } from '@material-ui/core';
+
+import Dialog from '@material-ui/core/Dialog';
+import DialogActions from '@material-ui/core/DialogActions';
+import DialogTitle from '@material-ui/core/DialogTitle';
+import DialogContent from '@material-ui/core/DialogContent';
+import Button from '@material-ui/core/Button';
+import Typography from '@material-ui/core/Typography';
 
 class CustomerDelete extends React.Component {
+
+  constructor(props) {
+    super(props);
+    this.state = {
+      open: false
+    }
+  }
+
+  handleClickOpen = () => {
+    this.setState({
+      open: true
+    })
+  }
+
+  handleClose = () => {
+    this.setState({
+      open: false
+    })
+  }
+
 
   deletedCustomer(id) {
     const url = `/api/customers/${id}`;
@@ -11,15 +37,34 @@ class CustomerDelete extends React.Component {
     this.props.stateRefresh();
   }
 
-
   render() {
     return (
-      <Button onClick={(e) => {this.deletedCustomer(this.props.id)}} >
-        削除
-      </Button>
+      <div>
+        <Button variant='contained' color='secondary' onClick={this.handleClickOpen} >
+          削除
+        </Button>
+        <Dialog open={this.state.open} onClose={this.handleClose}>
+          <DialogTitle onClose={this.handleClose}>
+          　削除
+          </DialogTitle>
+          <DialogContent>
+            <Typography gutterBottom>
+              選択したお客さんの情報が削除されます。
+            </Typography>
+          </DialogContent>
+            
+          <DialogActions>
+            <Button variant='contained' color='primary' onClick={(e) => {this.deletedCustomer(this.props.id)}}>
+              ・削除
+            </Button>
+            <Button variant='outlined' color='primary' onClick={this.handleClose} >
+              閉じる
+            </Button>
+          </DialogActions>
+        </Dialog>
+      </ div>
     )
   }
-
 }
 
 export default CustomerDelete;
